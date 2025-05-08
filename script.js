9bff567bb4f6b0e59f9dc5dd4123704e3eb9c964
KARAN VAGHELA <vaghelakaran8599@gmail.com>
2025-05-09 00:02:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const toggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+  body.classList.add('dark-mode');
+  toggle.checked = true;
+}
+
+toggle.addEventListener('change', () => {
+  body.classList.toggle('dark-mode');
+  const theme = body.classList.contains('dark-mode') ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+});
+
